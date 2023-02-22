0e9356a571366f38fd750b4a4e904c7077965501
margaritabjerrum <margarita.juskyte@gmail.com>
2023-02-22 09:45:13 +0200

    feat: yup validation schema for create bike handler

--- src/controllers/bikes-controller/create-bike.ts
+++ src/controllers/bikes-controller/create-bike.ts
@@ -1,37 +1,82 @@
 import { RequestHandler } from 'express';
+import {
+  object, string, number, array, ObjectSchema,
+} from 'yup';
 import BikeModel from './bike-model';
 import bikes from './bikes-data';
+
+const bikeDataValidationSchema: ObjectSchema<BikeData> = object({
+  brand: string()
+    .required('Brand is required')
+    .min(2, 'Must be more than 2 symbols')
+    .max(32, 'Must be less than 32 symbols'),
+
+  model: string()
+    .required('Model is required')
+    .min(2, 'Must be more than 2 symbols')
+    .max(32, 'Must be less than 32 symbols'),
+
+  year: number()
+    .required('Year is required')
+    .integer('Year must be integer')
+    .min(1950, 'Year must be more than 1950')
+    .max(2023, 'Year cannot be more than 2023'),
+
+  price: number()
+    .required('price is required')
+    .positive('price must be positive')
+    .test(
+      'isPrice',
+      'icorrect price format',
+      (val) => Number(val.toFixed(2)) === val,
+    ),
+
+  stats:
+    object({
+      engine: string()
+        .required('Engine is required')
+        .min(2, 'Must be more than 2 symbols')
+        .max(32, 'Must be less than 32 symbols'),
+      power: string()
+        .required('Power is required')
+        .min(2, 'Must be more than 2 symbols')
+        .max(32, 'Must be less than 32 symbols'),
+      seatHeight: string()
+        .required('Seat Height is required')
+        .min(2, 'Must be more than 2 symbols')
+        .max(32, 'Must be less than 32 symbols'),
+      weight: string()
+        .required('Weight is required')
+        .min(2, 'Must be more than 2 symbols')
+        .max(32, 'Must be less than 32 symbols'),
+    })
+      .required('Stats is required'),
+
+  images: array()
+    .of(string().required())
+    .required('Is required')
+    .min(1, 'images must have at least one image'),
+}).strict(true);
 
 const isBikeData = (
   potentialBikeData: PartialBikeData | BikeData,
 ): potentialBikeData is BikeData => {
-  const {
-    brand, model, year, price, stats, images,
-  } = potentialBikeData;
-
-  if (typeof brand !== 'string') return false;
-  if (typeof model !== 'string') return false;
-  if (typeof year !== 'number') return false;
-  if (typeof price !== 'number') return false;
-  if (stats === null || typeof stats !== 'object') return false;
-  if (typeof stats.engine !== 'string') return false;
-  if (typeof stats.power !== 'string') return false;
-  if (typeof stats.seatHeight !== 'string') return false;
-  if (typeof stats.weight !== 'string') return false;
-  if (!Array.isArray(images)) return false;
-  if (images.some((img) => typeof img !== 'string')) return false;
-
-  return true;
+  try {
+    bikeDataValidationSchema.validateSync(potentialBikeData);
+    return true;
+  } catch (error) {
+    return false;
+  }
 };
 
 type BikeData = Omit<BikeModel, 'id'>;
 type PartialBikeData = PartialRecursive<BikeData>;
 
 export const createBike: RequestHandler<
-{}, // Paramentrai
-BikeModel | ResponseError, // Atsakymo tipas
-PartialBikeData, // Body: gaunami duomenys
-{} // QueryParams: duomenis siunciant get uzklausas, pvz ?min=18max=18
+{},
+BikeModel | ResponseError,
+PartialBikeData,
+{}
 > = (req, res) => {
   const bikeData = req.body;
   if (!isBikeData(bikeData)) {
