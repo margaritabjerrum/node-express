d2a165a5a7bd936b241e8d9490d375e8cf3a12c2
margaritabjerrum <margarita.juskyte@gmail.com>
2023-03-08 13:45:55 +0200

    feat: authmiddleware

--- src/controllers/bikes-controller/index.ts
+++ src/controllers/bikes-controller/index.ts
@@ -1,17 +1,18 @@
-import express from 'express';
+import express, { RequestHandler } from 'express';
 import { getBike } from './queries/get-bike';
 import { getBikes } from './queries/get-bikes';
 import { createBike } from './mutations/create-bike';
 import { deleteBike } from './mutations/delete-bike';
 import { updateBike } from './mutations/update-bike';
+import authMiddleware from '../../middlewares/auth-middleware';
 
 const bikesRouter = express.Router();
 
 bikesRouter.get('/', getBikes);
 bikesRouter.get('/:id', getBike);
 
-bikesRouter.post('/', createBike);
-bikesRouter.delete('/:id', deleteBike);
-bikesRouter.patch('/:id', updateBike);
+bikesRouter.post('/', authMiddleware, createBike);
+bikesRouter.delete('/:id', authMiddleware, deleteBike as RequestHandler);
+bikesRouter.patch('/:id', authMiddleware, updateBike as RequestHandler);
 
 export default bikesRouter;
---
+++ src/middlewares/auth-middleware.ts
@@ -0,0 +1,25 @@
+import { RequestHandler } from 'express';
+import ErrorService, { AuthorizationError } from 'services/error-service';
+import TokenService from 'services/token-service';
+
+const authMiddleware: RequestHandler = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  try {
+    if (authorization === undefined) throw new AuthorizationError();
+
+    const token = authorization.split(' ')[1];
+    if (token === undefined) throw new AuthorizationError();
+
+    const authData = TokenService.decode(token);
+    if (authData === null) throw new AuthorizationError();
+
+    req.authData = authData;
+    next();
+  } catch (err) {
+    const [status, errorResponse] = ErrorService.handleError(err);
+    res.status(status).json(errorResponse);
+  }
+};
+
+export default authMiddleware;
