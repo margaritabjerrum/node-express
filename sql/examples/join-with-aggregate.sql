a7256f13e6166a6a4b11e698ee91d21b75263481
margaritabjerrum <margarita.juskyte@gmail.com>
2023-03-09 14:02:40 +0200

    feat: updated join-with-agregate sql query to include user data

--- sql/examples/join-with-aggregate.sql
+++
@@ -1,7 +0,0 @@
-SELECT h.id, h.title, JSON_OBJECT('country', l.country, 'city', l.city) as location, l.city, h.price, h.rating, JSON_ARRAYAGG(i.src)
-FROM images as i
-LEFT JOIN houses as h
-ON i.houseId = h.id
-LEFT JOIN  locations as l
-ON h.locationId = l.id
-GROUP BY h.id;
---
+++ sql/joins/join-with-aggregate.sql
@@ -0,0 +1,27 @@
+SELECT 
+	b.id, 
+  b.brand, 
+  b.model, 
+  b.year, 
+  b.price, 
+  JSON_OBJECT(
+		'engine', s.engine, 
+    'power', s.power, 
+    'seatHeight', s.seat_height, 
+    'weight', s.weight
+    ) as stats, 
+  IF(COUNT(i.id) = 0, JSON_ARRAY(), JSON_ARRAYAGG(i.src)) as images
+  JSON_OBJECT(
+    'id', u.id,
+		'firstname', u.firstname,
+		'lastname', u.lastname,
+		'email', u.email
+	) as owner
+FROM images as i
+LEFT JOIN bikes as b
+ON i.bikeId = b.id
+LEFT JOIN  stats as s
+ON b.statsId = s.id
+LEFT JOIN users as u
+ON u.id = b.ownerId
+GROUP BY b.id
